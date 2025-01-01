dd76e61c8569cdcc41c1999a4b37660966f54d7a
SteveLee <qwersteve07@gmail.com>
2025-01-01 19:57:21 +0800

    feat: remove url service

--- router.js
+++ router.js
@@ -1,13 +1,13 @@
 const Router = require("koa-router");
-const urlController = require("./controller/url");
+// const urlController = require("./controller/url");
 const flowController = require("./controller/flow");
 const router = new Router();
 
 router.get("/", (ctx) => {
   ctx.status = 404;
 });
-router.post("/generateUrl", urlController.generateUrl);
-router.get("/search/:hash", urlController.searchHash);
+// router.post("/generateUrl", urlController.generateUrl);
+// router.get("/search/:hash", urlController.searchHash);
 router.get("/flow/fetch-static-schedule", flowController.fetchStaticSchedule);
 router.post("/flow/fetch-site-schedule", flowController.fetchSiteSchedule);
 router.post("/flow/check-in-class", flowController.checkInClass);
--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const Koa = require("koa");
 const { koaBody } = require("koa-body");
 const cors = require("@koa/cors");
 const router = require("./router");
-const redisService = require("./redis");
+// const redisService = require("./redis");
 const runCron = require("./cron");
 const app = new Koa();
 const port = 8080;
 
 (async () => {
-  await redisService.init();
+  // await redisService.init();
 
   runCron();
 
