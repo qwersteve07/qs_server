e904835306345c49e18864107d9edbad0172e308
SteveLee <qwersteve07@gmail.com>
2025-01-29 21:40:30 +0800

    feat: impl events generate

---
+++ utils/generateEvents.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+
+const {
+  getEventInEveryWeek,
+  getEventInSingleWeek,
+} = require("./getStaticEvents");
+const customEvents = require("../custom/latin-dance-events.json");
+
+function run() {
+  const flowMiniSocialEvents = getEventInEveryWeek("flow-mini-social", 1);
+  const flowFridaySocialEvents1 = getEventInSingleWeek(
+    "flow-friday-social",
+    5,
+    1
+  );
+  const flowBachataMiAmorEvents = getEventInSingleWeek(
+    "flow-bachata-mi-amor",
+    5,
+    2
+  );
+  const flowFridaySocialEvents2 = getEventInSingleWeek(
+    "flow-friday-social",
+    5,
+    3
+  );
+  const flowFridaySocialEvents3 = getEventInSingleWeek(
+    "flow-friday-social",
+    5,
+    4
+  );
+
+  const flowFridaySocialEvents4 = getEventInSingleWeek(
+    "flow-friday-social",
+    5,
+    5
+  );
+  const brassMonkeySocialEvents = getEventInEveryWeek(
+    "brass-monkey-latin-night",
+    2
+  );
+
+  const BarcadeBachataNightEvents = getEventInEveryWeek(
+    "barcade-bachata-night",
+    4
+  );
+
+  const flowZoukSocialEvents = getEventInEveryWeek("flow-zouk-social", 4);
+  const CopaFridaySocialEvents = getEventInEveryWeek("copa-friday-social", 5);
+
+  writeFile([
+    ...flowMiniSocialEvents,
+    ...flowFridaySocialEvents1,
+    ...flowBachataMiAmorEvents,
+    ...flowFridaySocialEvents2,
+    ...flowFridaySocialEvents3,
+    ...flowFridaySocialEvents4,
+    ...brassMonkeySocialEvents,
+    ...BarcadeBachataNightEvents,
+    ...flowZoukSocialEvents,
+    ...CopaFridaySocialEvents,
+    ...customEvents,
+  ]);
+}
+
+function writeFile(data) {
+  fs.writeFile(
+    "./data/latin-dance-events.json",
+    JSON.stringify(data),
+    (err) => {
+      if (err) console.log(err);
+      else {
+        console.log("File written successfully");
+      }
+    }
+  );
+}
+
+run();
---
+++ utils/getStaticEvents.js
@@ -0,0 +1,87 @@
+const dayjs = require("dayjs");
+const socialData = require("../custom/social.json");
+var weekOfYear = require("dayjs/plugin/weekOfYear");
+var utc = require("dayjs/plugin/utc");
+var timezone = require("dayjs/plugin/timezone");
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(weekOfYear);
+dayjs.tz.setDefault("Asia/Taipei");
+
+function getEventInEveryWeek(name, weekday) {
+  const currentYear = dayjs().year();
+  const currentWeek = dayjs().week(1);
+  let currentDay = currentWeek.day(weekday);
+  const list = [];
+  const selectSocial = socialData[name];
+
+  // 透過 dayjs().week(1) 選到的第一天可能為前年
+  // 需要在 +7 到當年的第一個 weekday
+  if (currentDay.year() < currentYear) {
+    currentDay = currentDay.add(7, "day");
+  }
+
+  while (currentDay.year() === currentYear) {
+    list.push({
+      //   title: selectSocial.title,
+      start: `${currentDay.format("YYYY-MM-DDT")}${selectSocial["startAt"]}`,
+      end: `${currentDay.format("YYYY-MM-DDT")}${selectSocial["endAt"]}`,
+      ...selectSocial,
+    });
+
+    currentDay = currentDay.add(7, "day");
+  }
+
+  return list;
+}
+
+function getEventInSingleWeek(name, weekday, weekCount) {
+  const currentYear = dayjs().year();
+  let currentMonth = dayjs().month(0);
+  let currentDay = currentMonth.date(1);
+  let currentWeekCount = 1;
+  const list = [];
+  const selectSocial = socialData[name];
+  let loopContinue = false;
+
+  while (currentDay.year() === currentYear) {
+    while (currentDay.$W !== weekday) {
+      currentDay = currentDay.add(1, "day");
+    }
+
+    while (currentWeekCount !== weekCount) {
+      currentDay = currentDay.add(1, "week");
+      if (currentDay.$M !== currentMonth.$M) {
+        currentMonth = currentMonth.add(1, "month");
+        currentDay = currentMonth.date(1);
+        currentWeekCount = 1;
+        loopContinue = true;
+        break;
+      }
+      currentWeekCount++;
+    }
+
+    if (loopContinue) {
+      loopContinue = false;
+      continue;
+    }
+
+    list.push({
+      title: selectSocial.name,
+      start: `${currentDay.format("YYYY-MM-DDT")}${selectSocial["startAt"]}`,
+      end: `${currentDay.format("YYYY-MM-DDT")}${selectSocial["endAt"]}`,
+      ...selectSocial,
+    });
+
+    currentMonth = currentMonth.add(1, "month");
+    currentDay = currentMonth.date(1);
+    currentWeekCount = 1;
+  }
+
+  return list;
+}
+
+module.exports = {
+  getEventInEveryWeek,
+  getEventInSingleWeek,
+};
