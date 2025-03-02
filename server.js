7cac7f85f63d956bcec2f61630204f61e7bdb940
SteveLee <qwersteve07@gmail.com>
2025-03-02 13:43:12 +0800

    feat: add cors

--- server.js
+++ server.js
@@ -12,8 +12,23 @@ const port = 8080;
 
   runCron();
 
+  const allowedOrigins = [
+    "https://fast-flow-three.vercel.app/",
+    "http://fast-flow-three.vercel.app/",
+    "https://social-dance.tw",
+    "http://social-dance.tw",
+    "http://localhost:8000",
+  ];
+
   app
-    .use(cors())
+    .use(
+      cors({
+        origin: (ctx) => {
+          const requestOrigin = ctx.request.header.origin;
+          return allowedOrigins.includes(requestOrigin) ? requestOrigin : "";
+        },
+      })
+    )
     .use(koaBody({ multipart: true }))
     .use(router.routes())
     .use(router.allowedMethods())
