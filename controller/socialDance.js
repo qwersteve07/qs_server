16d89cbafc2215e2c3d148f9807f1116ee393c24
SteveLee <qwersteve07@gmail.com>
2025-04-09 23:41:04 +0800

    fix: typo

--- controller/socialDance.js
+++ controller/socialDance.js
@@ -72,7 +72,7 @@ const fetchEventsJson = async () => {
   const fileData = await fs.readFile(filePath, "utf-8");
   const data = JSON.parse(fileData);
 
-  return data.result.filter((d) => {
+  return data.filter((d) => {
     return dayjs(d.date).isSameOrAfter(dayjs().subtract(1, "d"));
   });
 };
