15edb4b49537be40cb49131b8900e4792f512266
SteveLee <qwersteve07@gmail.com>
2025-10-14 01:34:54 +0800

    feat: impl refresh token api

--- controller/socialDance.js
+++ controller/socialDance.js
@@ -16,7 +16,31 @@ const users = [
     password: bcrypt.hashSync("asdfjames07", 10),
   },
 ];
-const SECRET_KEY = "this-is-my-damn-secret-key";
+const ACCESS_TOKEN_SECRET = "this-is-my-damn-access-token-secret";
+const REFRESH_TOKEN_SECRET = "this-is-my-damn-refresh-token-secret";
+
+let refreshTokens = [];
+
+function generateTokens(user) {
+  const accessToken = jwt.sign(
+    { id: user.id, username: user.username },
+    ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
+  const refreshToken = jwt.sign(
+    { id: user.id, username: user.username },
+    REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+
+  refreshTokens.push(refreshToken);
+
+  return { accessToken, refreshToken };
+}
 
 function validateAuth(ctx) {
   const authHeader = ctx.headers.authorization;
@@ -30,12 +54,12 @@ function validateAuth(ctx) {
   try {
     const token = authHeader.split(" ")[1];
 
-    const result = jwt.verify(token, SECRET_KEY);
+    const result = jwt.verify(token, ACCESS_TOKEN_SECRET);
 
     if (!result) throw "";
     return { ok: true };
   } catch (error) {
-    ctx.status = 403;
+    ctx.status = 401;
     ctx.body = { error: "Invalid token" };
   }
 }
@@ -51,10 +75,38 @@ const login = async (ctx) => {
     return;
   }
 
-  const token = jwt.sign({ id: user.id, username: user.username }, SECRET_KEY, {
-    expiresIn: "1h",
-  });
-  ctx.body = { token };
+  ctx.status = 200;
+  const tokens = generateTokens(user);
+  ctx.body = { data: tokens };
+};
+
+const refresh = async (ctx) => {
+  const body = ctx.request.body;
+  const parseData = JSON.parse(body);
+
+  if (
+    !parseData.refreshToken ||
+    !refreshTokens.includes(parseData.refreshToken)
+  ) {
+    ctx.status = 403;
+    ctx.body = { error: "Invalid refresh token1" };
+    return;
+  }
+
+  try {
+    const user = jwt.verify(parseData.refreshToken, REFRESH_TOKEN_SECRET);
+    const tokens = generateTokens(user);
+    refreshTokens = refreshTokens.filter(
+      (token) => token !== parseData.refreshToken
+    );
+    refreshTokens.push(tokens.refreshToken);
+    ctx.status = 200;
+    ctx.body = { data: tokens };
+  } catch (error) {
+    console.log(error);
+    ctx.status = 403;
+    ctx.body = { error: "Invalid refresh token2" };
+  }
 };
 
 const fetchEventsJson = async () => {
@@ -85,6 +137,18 @@ const fetchEvents = async (ctx) => {
   ctx.body = { result };
 };
 
+const fetchEventsList = async (ctx) => {
+  const validateResult = validateAuth(ctx);
+
+  if (!validateResult?.ok) {
+    return validateResult;
+  }
+
+  const result = await fetchEventsJson();
+  ctx.status = 201;
+  ctx.body = { result };
+};
+
 const createEvent = async (ctx) => {
   const validateResult = validateAuth(ctx);
 
@@ -110,8 +174,6 @@ const fetchEvent = async (ctx) => {
 
 const updateEvent = async (ctx) => {
   const validateResult = validateAuth(ctx);
-
-  console.log(validateResult);
 
   if (!validateResult?.ok) {
     return validateResult;
@@ -161,9 +223,11 @@ const deleteEvent = async (ctx) => {
 
 module.exports = {
   fetchEvents,
+  fetchEventsList,
   fetchEvent,
   createEvent,
   updateEvent,
   deleteEvent,
   login,
+  refresh,
 };
--- router.js
+++ router.js
@@ -13,11 +13,14 @@ router.get("/flow/fetch-static-schedule", flowController.fetchStaticSchedule);
 router.post("/flow/fetch-site-schedule", flowController.fetchSiteSchedule);
 router.post("/flow/check-in-class", flowController.checkInClass);
 router.post("/flow/check-out-class", flowController.checkOutClass);
+
 router.get("/social-dance/events", socialDanceController.fetchEvents);
+router.get("/social-dance/eventsList", socialDanceController.fetchEventsList);
 router.post(`/social-dance/events`, socialDanceController.createEvent);
 router.get(`/social-dance/events/:id`, socialDanceController.fetchEvent);
 router.put(`/social-dance/events/:id`, socialDanceController.updateEvent);
 router.delete(`/social-dance/events/:id`, socialDanceController.deleteEvent);
 router.post("/social-dance/login", socialDanceController.login);
+router.post("/social-dance/refresh", socialDanceController.refresh);
 
 module.exports = router;
