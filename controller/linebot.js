4da22e854bf5bb15ea77d95db2d4501e6a098fed
sslee02 <Steve-SH.Lee@nanshan.com.tw>
2026-01-30 11:33:44 +0800

    feat: remove parse body

--- controller/linebot.js
+++ controller/linebot.js
@@ -1,10 +1,8 @@
 const receiveMessage = async (ctx) => {
 	const body = ctx.request.body;
-	console.log(body);
-	const parseData = JSON.parse(body);
 
 	ctx.status = 200;
-	ctx.body = { message: "Data received", data: parseData };
+	ctx.body = { message: "Data received", data: body };
 };
 
 export default { receiveMessage };
