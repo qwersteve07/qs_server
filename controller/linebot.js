10c0ac28342fe12aa228c7f66ef356adef2de149
sslee02 <Steve-SH.Lee@nanshan.com.tw>
2026-01-30 11:31:16 +0800

    feat: change the return HTTP code

--- controller/linebot.js
+++ controller/linebot.js
@@ -2,7 +2,7 @@ const receiveMessage = async (ctx) => {
 	const body = ctx.request.body;
 	const parseData = JSON.parse(body);
 
-	ctx.status = 201;
+	ctx.status = 200;
 	ctx.body = { message: "Data received", data: parseData };
 };
 
